d1c9494565f6eb5d5a757ca1e5a2a77c6d412386
Boris Cherny <boris@performancejs.com>
2019-02-24 10:33:22 -0800

    rm try datatype

--- src/ch06/exercises.ts
+++ src/ch06/exercises.ts
@@ -1,4 +1,4 @@
-// 1. Design a way to handle errors for the following API, using one of the patterns from this chapter. In this API, every operation might fail — feel free to update the API’s method signatures to allow for failures (or don’t, if you prefer). Think about how you might perform a sequence of actions while handling errors that come up (eg. getting the logged in user’s ID, then getting their list of friends).
+// 1. Design a way to handle errors for the following API, using one of the patterns from this chapter. In this API, every operation might fail — feel free to update the API’s method signatures to allow for Errs (or don’t, if you prefer). Think about how you might perform a sequence of actions while handling errors that come up (eg. getting the logged in user’s ID, then getting their list of friends).
 
 type UserID = unknown
 
@@ -47,9 +47,3 @@ let friendsUserNames = api
   .getLoggedInUserID()
   .flatMap(api.getFriendIDs)
   .flatMap(_ => listOfOptionsToOptionOfList(_.map(api.getUserName)))
-
-// 2. [Hard] Implement the Try datatype. Try is a lot like Option, and lets you chain possibly-errored computations. Like Option, it has two cases: Success<T> (like Some<T>), and Failure<T> (like None). Success<T> represents a successful computation, and can be mapped with flatMap; Failure<T> represents an exception, and can be recovered from with recoverWith.
-
-// Unlike Option, Try doesn’t just give you a None when something failed; it instead keeps track of the first exception that happened, so that you can choose to either recover from it or let it fail. The same way that you can’t read a value directly from Option, so you can’t read the value and exceptions directly from Try; instead, you can call recoverWith with a function, and if the Try is a Failure<T>, recoverWith will call your function with the exception. Try's API looks like this:
-
-// TODO
