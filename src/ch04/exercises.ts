a0daf2fbf980e2ae30efa2009436c7a14929bfc2
Boris Cherny <boris@performancejs.com>
2019-03-02 11:49:38 -0800

    add answer for typesafe, order-independent builder

--- src/ch04/exercises.ts
+++ src/ch04/exercises.ts
@@ -106,4 +106,38 @@ new RequestBuilder()
 
 // 4b. [Harder] How would you change your design if you wanted to make this guarantee, but still let people call methods in any order?
 
-// TODO
+// (This answer courtesy of @albertywu)
+
+interface BuildableRequest {
+  data?: object
+  method: 'get' | 'post'
+  url: string
+}
+
+class RequestBuilder2 {
+  data?: object
+  method?: 'get' | 'post'
+  url?: string
+
+  setData(data: object): this & Pick<BuildableRequest, 'data'> {
+    return Object.assign(this, {data})
+  }
+
+  setMethod(method: 'get' | 'post'): this & Pick<BuildableRequest, 'method'> {
+    return Object.assign(this, {method})
+  }
+
+  setURL(url: string): this & Pick<BuildableRequest, 'url'> {
+    return Object.assign(this, {url})
+  }
+
+  build(this: BuildableRequest) {
+    return this
+  }
+}
+
+new RequestBuilder2()
+  .setData({})
+  .setMethod('post') // Try removing me!
+  .setURL('bar') // Try removing me!
+  .build()
